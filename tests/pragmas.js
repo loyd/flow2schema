e44e49e9662158877c742b94df74f0deaee3be70
Paul Loyd <pavelko95@gmail.com>
2017-10-29 16:22:27 +0300

    Add tests for pragmas

---
+++ tests/pragmas.js
@@ -0,0 +1,75 @@
+type Type = {
+    // $avro int
+    a: number,
+    /* $avro long */
+    b: number,
+    //   $avro   float
+    c: number,
+    // $avro double
+    d: number,
+    /*   $avro   fixed       10   */
+    e: Buffer,
+};
+
+interface Interface {
+    // $avro int
+    a: number;
+    /* $avro long */
+    b: number;
+    //   $avro   float
+    c: number;
+    // $avro double
+    d: number;
+    /*   $avro   fixed       10   */
+    e: Buffer;
+}
+
+class Class {
+    // $avro int
+    a: number;
+    /* $avro long */
+    b: number;
+    //   $avro   float
+    c: number;
+    // $avro double
+    d: number;
+    /*   $avro   fixed       10   */
+    e: Buffer;
+}
+
+// ###
+({
+    Type: {
+        type: 'record',
+        name: 'Type',
+        fields: [
+            {name: 'a', type: 'int'},
+            {name: 'b', type: 'long'},
+            {name: 'c', type: 'float'},
+            {name: 'd', type: 'double'},
+            {name: 'e', type: {type: 'fixed', size: 10}},
+        ],
+    },
+    Interface: {
+        type: 'record',
+        name: 'Interface',
+        fields: [
+            {name: 'a', type: 'int'},
+            {name: 'b', type: 'long'},
+            {name: 'c', type: 'float'},
+            {name: 'd', type: 'double'},
+            {name: 'e', type: {type: 'fixed', size: 10}},
+        ],
+    },
+    Class: {
+        type: 'record',
+        name: 'Class',
+        fields: [
+            {name: 'a', type: 'int'},
+            {name: 'b', type: 'long'},
+            {name: 'c', type: 'float'},
+            {name: 'd', type: 'double'},
+            {name: 'e', type: {type: 'fixed', size: 10}},
+        ],
+    },
+});
