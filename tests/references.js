16632df5edb397376fadde69248696a0536ab78e
Paul Loyd <pavelko95@gmail.com>
2017-11-09 14:56:26 +0300

    Small refactoring

--- tests/references.js
+++ tests/references.js
@@ -3,19 +3,16 @@ type A = string;
 type Type = {
     a: A,
     b: A[],
-    c: A<number>,
 };
 
 interface Interface {
     a: A;
     b: A[];
-    c: A<number>;
 }
 
 class Class {
     a: A;
     b: A[];
-    c: A<number>;
 }
 
 export {Type, Interface, Class};
