87979aa328a7a84c725814dd7119fdb6316b8405
Paul Loyd <pavelko95@gmail.com>
2017-11-28 20:43:22 +0300

    Small refactoring

--- src/globals.js
+++ src/globals.js
@@ -1,40 +1,38 @@
 import {invariant} from './utils';
 import type {Type} from './types';
 
-export default {
-    Object(params: (?Type)[]): ?Type {
-        invariant(params.length === 0);
+function object(params: (?Type)[]): ?Type {
+    invariant(params.length === 0);
 
-        return {
-            kind: 'map',
-            keys: {kind: 'mixed'},
-            values: {kind: 'mixed'},
-        };
-    },
-    Buffer(params: (?Type)[]): ?Type {
-        invariant(params.length === 0);
-
-        return {
-            kind: 'reference',
-            to: ['Buffer'],
-        };
-    },
-    Array(params: (?Type)[]): ?Type {
-        invariant(params.length === 1);
-        invariant(params[0]);
-
-        return {
-            kind: 'array',
-            items: params[0],
-        };
-    },
-    $ReadOnlyArray(params: (?Type)[]): ?Type {
-        invariant(params.length === 1);
-        invariant(params[0]);
-
-        return {
-            kind: 'array',
-            items: params[0],
-        };
-    },
+    return {
+        kind: 'map',
+        keys: {kind: 'mixed'},
+        values: {kind: 'mixed'},
+    };
 }
+
+function buffer(params: (?Type)[]): ?Type {
+    invariant(params.length === 0);
+
+    return {
+        kind: 'reference',
+        to: ['Buffer'],
+    };
+}
+
+function array(params: (?Type)[]): ?Type {
+    invariant(params.length === 1);
+    invariant(params[0]);
+
+    return {
+        kind: 'array',
+        items: params[0],
+    };
+}
+
+export default {
+    Object: object,
+    Buffer: buffer,
+    Array: array,
+    $ReadOnlyArray: array,
+};
