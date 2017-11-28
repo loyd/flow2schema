d24a640ad5894f5377ad1b6b13e7eb66a37c974b
Paul Loyd <pavelko95@gmail.com>
2017-11-28 20:29:57 +0300

    Support for Array<T> and $ReadOnlyArray<T>

--- src/globals.js
+++ src/globals.js
@@ -1,8 +1,29 @@
+import {invariant} from './utils';
+import type {Type} from './types';
+
 export default {
     Buffer() {
         return {
             kind: 'reference',
-            to: ["Buffer"],
+            to: ['Buffer'],
+        };
+    },
+    Array(params: (?Type)[]): ?Type {
+        invariant(params.length === 1);
+        invariant(params[0]);
+
+        return {
+            kind: 'array',
+            items: params[0],
+        };
+    },
+    $ReadOnlyArray(params: (?Type)[]): ?Type {
+        invariant(params.length === 1);
+        invariant(params[0]);
+
+        return {
+            kind: 'array',
+            items: params[0],
         };
     },
 }
--- tests/samples/arrays.js
+++ tests/samples/arrays.js
@@ -1,13 +1,19 @@
 type Type = {
     a: string[],
+    b: Array<string>,
+    c: $ReadOnlyArray<string>,
 };
 
 interface Interface {
     a: string[];
+    b: Array<string>;
+    c: $ReadOnlyArray<string>;
 }
 
 class Class {
     a: string[];
+    b: Array<string>;
+    c: $ReadOnlyArray<string>;
 }
 
 export {Type, Interface, Class};
