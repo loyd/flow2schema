e24a73b03b074967f95b4eec883ade31f7de36b9
Paul Loyd <pavelko95@gmail.com>
2018-11-21 17:31:48 +0300

    Add selectiveImport sample

---
+++ tests/samples/selectiveImport/modules/first.js
@@ -0,0 +1,15 @@
+export type A = {
+    a: number;
+}
+
+export type IgnoredType = {
+    ignored: number,
+};
+
+export interface IgnoredInterface {
+    ignored: number;
+}
+
+export default class IgnoredDefault {
+    ignored: number;
+}
